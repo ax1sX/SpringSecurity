e8afdd03cae2298f11fa3f4b8f0821eaf2efc0aa
AxisX <15010255872@163.com>
2022-07-14 10:55:34 +0800

    Add CVE-2018-1270 RCE WebSocket with STOMP

---
+++ CVE-2018-1270/src/main/java/hello/Greeting.java
@@ -0,0 +1,18 @@
+package hello;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
