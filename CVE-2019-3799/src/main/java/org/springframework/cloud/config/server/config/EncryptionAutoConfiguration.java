0705c797604254ecc720ce19376aa3d5b2a276f6
AxisX <15010255872@163.com>
2022-07-19 15:18:36 +0800

    Add CVE-2019-3799 Directory Traversal

---
+++ CVE-2019-3799/src/main/java/org/springframework/cloud/config/server/config/EncryptionAutoConfiguration.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2002-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.config.server.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.bootstrap.encrypt.KeyProperties;
+import org.springframework.cloud.bootstrap.encrypt.KeyProperties.KeyStore;
+import org.springframework.cloud.bootstrap.encrypt.RsaProperties;
+import org.springframework.cloud.config.server.encryption.CipherEnvironmentEncryptor;
+import org.springframework.cloud.config.server.encryption.EnvironmentEncryptor;
+import org.springframework.cloud.config.server.encryption.KeyStoreTextEncryptorLocator;
+import org.springframework.cloud.config.server.encryption.LocatorTextEncryptor;
+import org.springframework.cloud.config.server.encryption.SingleTextEncryptorLocator;
+import org.springframework.cloud.config.server.encryption.TextEncryptorLocator;
+import org.springframework.cloud.context.encrypt.EncryptorFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.security.rsa.crypto.KeyStoreKeyFactory;
+import org.springframework.security.rsa.crypto.RsaAlgorithm;
+import org.springframework.security.rsa.crypto.RsaSecretEncryptor;
+import org.springframework.util.StringUtils;
+
+/**
+ * Auto configuration for text encryptors and environment encryptors (non-web stuff).
+ * Users can provide beans of the same type as any or all of the beans defined here in
+ * application code to override the default behaviour.
+ *
+ * @author Bartosz Wojtkiewicz
+ * @author Rafal Zukowski
+ * @author Dave Syer
+ *
+ */
+@Configuration
+@EnableConfigurationProperties(KeyProperties.class)
+@Import({ SingleTextEncryptorConfiguration.class,
+		DefaultTextEncryptorConfiguration.class })
+public class EncryptionAutoConfiguration {
+
+	@Configuration
+	@ConditionalOnProperty(value = "spring.cloud.config.server.encrypt.enabled", matchIfMissing = true)
+	protected static class EncryptorConfiguration {
+
+		@Autowired(required = false)
+		private TextEncryptorLocator locator;
+
+		@Autowired
+		private TextEncryptor encryptor;
+
+		@Bean
+		@ConditionalOnMissingBean
+		public EnvironmentEncryptor environmentEncryptor() {
+			TextEncryptorLocator locator = this.locator;
+			if (locator == null) {
+				locator = new SingleTextEncryptorLocator(encryptor);
+			}
+			return new CipherEnvironmentEncryptor(locator);
+		}
+
+	}
+
+	@Configuration
+	@ConditionalOnClass(RsaSecretEncryptor.class)
+	@ConditionalOnProperty(prefix = "encrypt.key-store", value = "location", matchIfMissing = false)
+	protected static class KeyStoreConfiguration {
+
+		@Autowired
+		private KeyProperties key;
+
+		@Autowired
+		private RsaProperties rsaProperties;
+
+		@Bean
+		@ConditionalOnMissingBean
+		public TextEncryptorLocator textEncryptorLocator() {
+			KeyStore keyStore = this.key.getKeyStore();
+			KeyStoreTextEncryptorLocator locator = new KeyStoreTextEncryptorLocator(
+					new KeyStoreKeyFactory(keyStore.getLocation(),
+							keyStore.getPassword().toCharArray()),
+					keyStore.getSecret(), keyStore.getAlias());
+			RsaAlgorithm algorithm = this.rsaProperties.getAlgorithm();
+			locator.setRsaAlgorithm(algorithm);
+			locator.setSalt(this.rsaProperties.getSalt());
+			locator.setStrong(this.rsaProperties.isStrong());
+			return locator;
+		}
+
+	}
+
+}
+
+@ConditionalOnBean(TextEncryptor.class)
+@ConditionalOnMissingBean(TextEncryptorLocator.class)
+@Configuration
+class SingleTextEncryptorConfiguration {
+
+	@Autowired
+	private TextEncryptor encryptor;
+
+	@Bean
+	public SingleTextEncryptorLocator textEncryptorLocator() {
+		return new SingleTextEncryptorLocator(encryptor);
+	}
+
+}
+
+@ConditionalOnMissingBean(TextEncryptor.class)
+@Configuration
+class DefaultTextEncryptorConfiguration {
+
+	@Autowired
+	private KeyProperties key;
+
+	@Autowired(required = false)
+	private TextEncryptorLocator locator;
+
+	@Bean
+	public TextEncryptor defaultTextEncryptor() {
+		if (locator != null) {
+			return new LocatorTextEncryptor(locator);
+		}
+		if (StringUtils.hasText(this.key.getKey())) {
+			return new EncryptorFactory(this.key.getSalt()).create(this.key.getKey());
+		}
+		return Encryptors.noOpText();
+	}
+
+}
---
+++ CVE-2019-3799/src/main/java/org/springframework/cloud/config/server/resource/ResourceController.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.config.server.resource;
+
+import static org.springframework.cloud.config.server.support.EnvironmentPropertySource.prepareEnvironment;
+import static org.springframework.cloud.config.server.support.EnvironmentPropertySource.resolvePlaceholders;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.cloud.config.environment.Environment;
+import org.springframework.cloud.config.server.environment.EnvironmentRepository;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UrlPathHelper;
+
+/**
+ * An HTTP endpoint for serving up templated plain text resources from an underlying
+ * repository. Can be used to supply config files for consumption by a wide variety of
+ * applications and services. A {@link ResourceRepository} is used to locate a
+ * {@link Resource}, specific to an application, and the contents are transformed to text.
+ * Then an {@link EnvironmentRepository} is used to supply key-value pairs which are used
+ * to replace placeholders in the resource text.
+ *
+ * @author Dave Syer
+ * @author Daniel Lavoie
+ *
+ */
+@RestController
+@RequestMapping(method = RequestMethod.GET, path = "${spring.cloud.config.server.prefix:}")
+public class ResourceController {
+
+	private ResourceRepository resourceRepository;
+
+	private EnvironmentRepository environmentRepository;
+
+	private UrlPathHelper helper = new UrlPathHelper();
+
+	public ResourceController(ResourceRepository resourceRepository,
+			EnvironmentRepository environmentRepository) {
+		this.resourceRepository = resourceRepository;
+		this.environmentRepository = environmentRepository;
+		this.helper.setAlwaysUseFullPath(true);
+	}
+
+	@RequestMapping("/{name}/{profile}/{label}/**")
+	public String retrieve(@PathVariable String name, @PathVariable String profile,
+			@PathVariable String label, HttpServletRequest request,
+			@RequestParam(defaultValue = "true") boolean resolvePlaceholders)
+			throws IOException {
+		String path = getFilePath(request, name, profile, label);
+		return retrieve(name, profile, label, path, resolvePlaceholders);
+	}
+
+	@RequestMapping(value = "/{name}/{profile}/**", params = "useDefaultLabel")
+	public String retrieve(@PathVariable String name, @PathVariable String profile,
+			HttpServletRequest request,
+			@RequestParam(defaultValue = "true") boolean resolvePlaceholders)
+			throws IOException {
+		String path = getFilePath(request, name, profile, null);
+		return retrieve(name, profile, null, path, resolvePlaceholders);
+	}
+
+	private String getFilePath(HttpServletRequest request, String name, String profile,
+			String label) {
+		String stem;
+		if(label != null ) {
+			stem = String.format("/%s/%s/%s/", name, profile, label);
+		}else {
+			stem = String.format("/%s/%s/", name, profile);
+		}
+		String path = this.helper.getPathWithinApplication(request);
+		path = path.substring(path.indexOf(stem) + stem.length());
+		return path;
+	}
+
+	synchronized String retrieve(String name, String profile, String label, String path,
+			boolean resolvePlaceholders) throws IOException {
+		if (name != null && name.contains("(_)")) {
+			// "(_)" is uncommon in a git repo name, but "/" cannot be matched
+			// by Spring MVC
+			name = name.replace("(_)", "/");
+		}
+		if (label != null && label.contains("(_)")) {
+			// "(_)" is uncommon in a git branch name, but "/" cannot be matched
+			// by Spring MVC
+			label = label.replace("(_)", "/");
+		}
+
+		// ensure InputStream will be closed to prevent file locks on Windows
+		try (InputStream is = this.resourceRepository.findOne(name, profile, label, path)
+				.getInputStream()) {
+			String text = StreamUtils.copyToString(is, Charset.forName("UTF-8"));
+			if (resolvePlaceholders) {
+				Environment environment = this.environmentRepository.findOne(name,
+						profile, label);
+				text = resolvePlaceholders(prepareEnvironment(environment), text);
+			}
+			return text;
+		}
+	}
+
+	@RequestMapping(value = "/{name}/{profile}/{label}/**", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	public synchronized byte[] binary(@PathVariable String name,
+			@PathVariable String profile, @PathVariable String label,
+			HttpServletRequest request) throws IOException {
+		String path = getFilePath(request, name, profile, label);
+		return binary(name, profile, label, path);
+	}
+
+	synchronized byte[] binary(String name, String profile, String label, String path)
+			throws IOException {
+		if (name != null && name.contains("(_)")) {
+			// "(_)" is uncommon in a git repo name, but "/" cannot be matched
+			// by Spring MVC
+			name = name.replace("(_)", "/");
+		}
+		if (label != null && label.contains("(_)")) {
+			// "(_)" is uncommon in a git branch name, but "/" cannot be matched
+			// by Spring MVC
+			label = label.replace("(_)", "/");
+		}
+		// TODO: is this line needed for side effects?
+		prepareEnvironment(this.environmentRepository.findOne(name, profile, label));
+		try (InputStream is = this.resourceRepository.findOne(name, profile, label, path)
+				.getInputStream()) {
+			return StreamUtils.copyToByteArray(is);
+		}
+	}
+
+	@ExceptionHandler(NoSuchResourceException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public void notFound(NoSuchResourceException e) {
+	}
+
+}
